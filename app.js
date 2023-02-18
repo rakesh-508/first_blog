fe4b6877f7186c4579e627ba23d9b95a66117bfa
Ande Rakesh <rakeshande99@gmail.com>
2023-02-18 11:37:16 +0530

    the changes are for the content1

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post("/compose", function(req, res){
 app.get("/posts/:postId", function(req, res){
 
 const requestedPostId = req.params.postId;
-  Post.findOne({_id: requestedPostId}, function(err, post){
+  Post.findMany({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
       content: post.content
